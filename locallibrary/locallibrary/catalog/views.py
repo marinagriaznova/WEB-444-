7914b2506c39cf9049639ef251328fab0a0d9e40
Marina <70626285+marinagriaznova@users.noreply.github.com>
2021-12-07 13:00:19 +0300

    Update views.py

--- locallibrary/locallibrary/catalog/views.py
+++ locallibrary/locallibrary/catalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import BookModel, BookInstanceModel, AuthorModel
 
-# Create your views here.
+def index(request):
+	num_books =
+	num_instances =
+	num_instances_available
+
+class ClassName(generic.ListView):
+	model = ClassNameModel
+	context_objects_name = 'books_list'
+	queryset = ClassNameModel.objects.all()
+	template_name = 'class_name_list.html'
