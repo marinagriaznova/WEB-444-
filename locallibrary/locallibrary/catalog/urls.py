d4b35e4d13fed9538eb2e184af3dbaadbd3df7ce
Marina <70626285+marinagriaznova@users.noreply.github.com>
2021-12-07 12:58:50 +0300

    fkdjfs

--- locallibrary/locallibrary/catalog/urls.py
+++ locallibrary/locallibrary/catalog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	path('', views.index,name='index')
+	path('class-name/', vews.ClassName.as_view(), name='Class name list')
     
 ]
